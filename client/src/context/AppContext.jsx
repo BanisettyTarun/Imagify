7f5085ad20349147aa9c8b06bd6e783e2f9d15c5
BanisettyTarun <148131422+BanisettyTarun@users.noreply.github.com>
2025-02-05 19:50:31 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -10,7 +10,7 @@ const AppContextProvider = (props)=>{
     const [showLogin, setShowLogin] = useState(false);
     const [token, setToken] = useState(localStorage.getItem('token'));
     const [credit, setCredit] = useState(false)
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://imagify-backend1-tmn1.onrender.com"
     const navigate = useNavigate();
     const loadCreditsData = async ()=>{
         try{
